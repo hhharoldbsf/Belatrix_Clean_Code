3b975032f5430cc96946a842487405d71e7a0d6c
Harold <hhernandez@belatrixsf.com>
2018-07-24 00:01:36 -0500

    Single Responsability finished.

---
+++ SOLID/01 SingleResponsability/Operations.cs
@@ -0,0 +1,13 @@
+﻿public static class Operations
+{
+    public static int Sum(int value1, int value2)
+    {
+        return value1 + value2;
+    }
+
+    public static bool IsBetween(int value, int left, int rigth)
+    {
+        if (value > left && value >= rigth) return true;
+        return false;
+    }
+}
--- SOLID/01 SingleResponsability/Sample.cs
+++ SOLID/01 SingleResponsability/Sample.cs
@@ -4,14 +4,19 @@ namespace SOLID._01_SingleResponsability
 {
     public class Sample
     {
-        public void Sum(int a, int b)
-        {
-            var c = a + b;
-            var m = "";
-            if (c > 0 && c <= 10) m = "Value between 0 and 10";
-            else if (c > 10 && c <= 20) m = "Value between 11 and 20";
-            else if (c > 20 && c <= 30) m = "Value between 11 and 20";
-            Console.WriteLine(string.Format("The sum is: {0} and is in range {1}", c, m));
+        public void Main(int value1, int value2)
+        {            
+            int sum = Operations.Sum(value1, value2);
+            string range = GetRange(sum);
+            Console.WriteLine(string.Format("The sum is: {0} and is in range {1}", sum, range));
         }
-    }
+
+        private string GetRange(int value)
+        {
+            if (Operations.IsBetween(value, 10, 20)) return "Value between 0 and 10";
+            else if (Operations.IsBetween(value, 10, 20)) return "Value between 11 and 20";
+            else if (Operations.IsBetween(value, 20, 30)) return "Value between 21 and 30";
+            else return "Value is greather than 30";
+        }
+    }    
 }
